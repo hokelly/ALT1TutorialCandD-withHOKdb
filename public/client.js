28962ab192515df842f23583a100941f50c4248c
hokelly <32305411+hokelly@users.noreply.github.com>
2025-11-09 21:59:35 +0000

    Update client.js
    pointing to STudents table. HOK

--- public/client.js
+++ public/client.js
@@ -41,7 +41,7 @@ function displayData() {
   // iterate through every row and add it to our page
   users.forEach(function (row) {
     const newListItem = document.createElement("li");
-    newListItem.innerHTML = row["first_name"] + " " + row["surname"];
+    newListItem.innerHTML = row["name"] + " " + row["age"] + " " + row["grade"]  + " " + row["city"]  + " " + row["photoPermission"];
     rowList.appendChild(newListItem);
   });
 }
